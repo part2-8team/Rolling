aa17e5ec30128f056e97b30209f3cd2e3f26b7ed
haze <skdml20022@gmail.com>
2024-04-30 19:02:15 +0900

    feat: #13 프로필 이미지 가져오는 api

--- src/api/etcApi.js
+++ src/api/etcApi.js
@@ -1,7 +1,14 @@
-import { BASE_URL } from "./requestBase";
+import { BASE_URL } from './requestBase';
 
 export const getBackgroundImages = async () => {
   const response = await fetch(`${BASE_URL}/background-images/`);
+  const body = await response.json();
+  const result = body["imageUrls"];
+  return result;
+};
+
+export const getProfileImages = async () => {
+  const response = await fetch(`${BASE_URL}/profile-images/`);
   const body = await response.json();
   const result = body["imageUrls"];
   return result;
--- src/api/requestBase.js
+++ src/api/requestBase.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://rolling-api.vercel.app";
-export const TEAM_NUM = "6-8";
+export const BASE_URL = 'https://rolling-api.vercel.app';
+export const TEAM_NUM = '6-8';
