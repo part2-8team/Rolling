003e8b34d9beae4132822643b7b5d3ab7f9509e3
JooKangsan <162934516+JooKangsan@users.noreply.github.com>
2024-05-01 23:59:44 +0900

    feat #2: 카드 로직및 css 구성

---
+++ src/pages/CardItems.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CardItems() {
+  
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default CardItems;
---
+++ test.js
@@ -0,0 +1,17 @@
+function createIncrement(incBy) {
+  let value = 0;
+
+  function increment() {
+    value += incBy;
+    console.log(value);
+  }
+
+  const message = `Current value is ${value}`;
+  function log() {
+    console.log(message);
+  }
+
+  return [increment, log];
+}
+
+const [increment, log] = createIncrement(1);
