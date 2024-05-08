a4da28e1a183d5aa9484c1834a586439a9a9ba93
haze <skdml20022@gmail.com>
2024-05-08 15:23:02 +0900

    refactor: 다른 페이지에서도 재사용하기 위해 SliderTitle -> SectionTitle로 명칭 변경

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import React from 'react';
 import MainPage from './pages/MainPage';
 import ListPage from './pages/ListPage';
 import Post from './pages/Post';
-// import PostId from './pages/PostId';
-// import PostIdEdit from "./pages/PostIdEdit";
-// import PostIdMessage from "./pages/PostIdMessage";
+import PostId from './pages/PostId';
+import PostIdEdit from './pages/PostIdEdit';
+import PostIdMessage from './pages/PostIdMessage';
 import styled from 'styled-components';
 
 function App() {
@@ -16,12 +16,12 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="list" element={<ListPage />} />
         <Route path="post" element={<Post />}>
-          {/* <Route index element={<PostId />} />
+          <Route index element={<PostId />} />
           <Route path=":id">
             <Route index element={<PostId />} />
             <Route path="message" element={<PostIdMessage />} />
             <Route path="edit" element={<PostIdEdit />} />
-          </Route> */}
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
