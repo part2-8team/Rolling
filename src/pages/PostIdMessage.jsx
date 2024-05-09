9a87779c0a5f168eebdd51478fa6d528d34a2abc
haze <skdml20022@gmail.com>
2024-05-09 14:58:55 +0900

    feat : select box 컴포넌트 생성

--- src/pages/PostIdMessage.jsx
+++ src/pages/PostIdMessage.jsx
@@ -4,9 +4,18 @@ import Header from '../components/Header';
 import SectionTitle from '../components/SectionTitle';
 import Button from '../components/Button/Button';
 import InputBox from '../components/InputBox';
+import SelectBox from '../components/SelectBox';
 import { createMessage } from '../api/messageApi';
 
 function PostIdMessage() {
+  const relationship = ['친구', '지인', '동료', '가족'];
+  const fonts = ['Noto Sans', 'Pretendard', '나눔명조', '나눔손글씨 손편지체'];
+  const formData = {};
+
+  const handleSubmit = () => {
+    createMessage();
+  };
+
   return (
     <>
       <Header />
@@ -20,15 +29,17 @@ function PostIdMessage() {
         </Section>
         <Section>
           <SectionTitle title="상대와의 관계" />
+          <SelectBox options={relationship} />
         </Section>
         <Section>
           <SectionTitle title="내용을 입력해 주세요" />
         </Section>
         <Section>
           <SectionTitle title="폰트 선택" />
+          <SelectBox options={fonts} />
         </Section>
 
-        <Button text="생성하기" width="100%" onClick={createMessage()} />
+        <Button text="생성하기" width="100%" onClick={handleSubmit} />
       </Container>
     </>
   );
@@ -36,9 +47,9 @@ function PostIdMessage() {
 
 export default PostIdMessage;
 
-const Container = styled.form`
+const Container = styled.form.attrs()`
   width: 720px;
-  margin: 112px auto 0 auto;
+  margin: 112px auto 24px auto;
   box-sizing: border-box;
 `;
 
